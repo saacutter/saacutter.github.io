421cf41648a415583f4ad53ed88c845ddab3c795
saacutter <168063553+saacutter@users.noreply.github.com>
2026-01-06 20:40:49 +0800

    Added templates for blog posts (no functionality yet)

--- deployment.py
+++ deployment.py
@@ -3,28 +3,60 @@ import datetime
 from jinja2 import Environment, FileSystemLoader
 
 POSTS = []
+ENV = Environment(loader=FileSystemLoader("templates"))
 
 def get_posts():
+    # Ensure that the global variable is used to avoid doing this multiple times
     global POSTS
+
+    # Walk the entire directory
     for root, dirs, files in os.walk("blog/"):
         for file in files:
-            POSTS.append(f'{root}{file}')
+            path = f'{root}{file}'
+            mtime = datetime.datetime.fromtimestamp(os.path.getctime(path))
+            POSTS.append({"title": file.replace(".md", ""), "path": path, "mtime": mtime})
+
+    # Sort the posts by their modification date (newest first)
+    POSTS.sort(key = lambda x: x["mtime"], reverse=True)
+
 
 def render_homepage():
-    env = Environment(loader=FileSystemLoader("templates"))
-    base = env.get_template("base.html")
-    homepage = env.get_template("_homepage.html")
-    output = base.render(title="saacutter.github.io", body = homepage.render(posts = POSTS))
+    global ENV
 
+    # Get the homepage template and render it
+    homepage = ENV.get_template("homepage.html")
+    output = homepage.render(posts=POSTS)
+
+    # Write the render to the index.html file
     with open("public/index.html", "w") as file:
         file.write(output)
 
     print("The homepage has successfully been created.")
 
-def render_post():
-    env = Environment(loader=FileSystemLoader("templates"))
-    base = env.get_template("base.html")
+
+def render_posts():
+    global ENV
+
+    # Store the post template in memory
+    post_template = ENV.get_template("post.html")
+
+    # Render every post
+    for post in POSTS:
+        # Render the post using the template
+        output = post_template.render(post=post)
+
+        # Create the path of the output render and make the directory
+        path = os.path.join("public", "blog", str(post["mtime"].year), str(post["mtime"].month), str(post["mtime"].day))
+        os.makedirs(path, exist_ok=True)
+
+        # Write the render to a file
+        with open(f"{os.path.join(path, post["title"])}.html", "w") as file:
+            file.write(output)
+        
+        print(f"The page for the post {post["title"]} has successfully been created.")
+
 
 if __name__ == '__main__':
     get_posts()
     render_homepage()
+    render_posts()
