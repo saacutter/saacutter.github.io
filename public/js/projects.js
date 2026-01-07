81e54e34f8b4852fcf22f1ba350467fca4d625e2
saacutter <168063553+saacutter@users.noreply.github.com>
2026-01-07 09:18:54 +0800

    Changed how the navbar is handled on blog posts (the categories now properly hover and link back to the designated page)

--- public/js/projects.js
+++ public/js/projects.js
@@ -1,35 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const url = new URL(window.location.href);
-
-    // Handle navbar categories
     const navbar = document.querySelectorAll('nav a');
-    const categories = document.querySelectorAll('div.container');
-    navbar.forEach((nav, i) => {
-        // Highlight the category on hover
-        nav.addEventListener('mouseover', () => nav.classList.add('navbar-selected'));
 
-        // Unhighlight the category on mouse exit
-        nav.addEventListener('mouseout', () => {if (!categories[i].checkVisibility()) nav.classList.remove('navbar-selected')});
+    if (window.location.pathname == "/") {
+        const url = new URL(window.location.href);
 
-        // Show the category elements on click
-        nav.addEventListener('click', () => {
-            // Unselect and hide all categories
-            navbar.forEach((n) => n.classList.remove('navbar-selected'));
-            categories.forEach((category) => category.style.display = 'none');
+        // Handle navbar categories
+        const categories = document.querySelectorAll('div.container');
+        navbar.forEach((nav, i) => {
+            // Highlight the category on hover
+            nav.addEventListener('mouseover', () => nav.classList.add('navbar-selected'));
 
-            // Select the clicked category
-            nav.classList.add('navbar-selected');
+            // Unhighlight the category on mouse exit
+            nav.addEventListener('mouseout', () => {if (!categories[i].checkVisibility()) nav.classList.remove('navbar-selected')});
 
-            // Make the corresponding category visible
-            categories[i].style.display = 'block';
+            // Show the category elements on click
+            nav.addEventListener('click', (e) => {
+                // Prevent default behaviour of anchor tags
+                e.preventDefault();
 
-            // Push the new URL to history
-            window.history.pushState({}, '', `?view=${nav.id}`)
-        })
-    });
+                // Unselect and hide all categories
+                navbar.forEach((n) => n.classList.remove('navbar-selected'));
+                categories.forEach((category) => category.style.display = 'none');
 
-    // Set the search parameters
-    const params = url.searchParams;
-    let view = params.get('view') || 'home';
-    document.querySelector(`#${view}`).click();
+                // Select the clicked category
+                nav.classList.add('navbar-selected');
+
+                // Make the corresponding category visible
+                categories[i].style.display = 'block';
+
+                // Push the new URL to history
+                window.history.pushState({}, '', `?view=${nav.id}`)
+            });
+        });
+
+        // Set the search parameters
+        const params = url.searchParams;
+        let view = params.get('view') || 'home';
+        if (document.querySelector(`#${view}`) == null) view = 'home';
+        document.querySelector(`#${view}`).click();
+    } else {
+        navbar.forEach((nav) => {
+            // Highlight the category on hover
+            nav.addEventListener('mouseover', () => nav.classList.add('navbar-selected'));
+
+            // Unhighlight the category on mouse exit
+            nav.addEventListener('mouseout', () => nav.classList.remove('navbar-selected'));
+        });
+    }
 });
