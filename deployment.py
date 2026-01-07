5fdda7b44df0534054d535434dac0e198952fdd5
saacutter <168063553+saacutter@users.noreply.github.com>
2026-01-07 20:32:26 +0800

    Added some basic blog functionality and moved the games section from "projects" to the top navigation bar

--- deployment.py
+++ deployment.py
@@ -1,6 +1,8 @@
 import os
 import datetime
 from jinja2 import Environment, FileSystemLoader
+import mistune
+import yaml
 
 POSTS = []
 ENV = Environment(loader=FileSystemLoader("templates"))
@@ -12,9 +14,14 @@ def get_posts():
     # Walk the entire directory
     for root, dirs, files in os.walk("blog/"):
         for file in files:
-            path = f'{root}{file}'
+            # Construct the path of the file as the root directory and filename
+            path = os.path.join(root, file)
+
+            # Get the modification date of the file
             mtime = datetime.datetime.fromtimestamp(os.path.getctime(path))
-            POSTS.append({"title": file.replace(".md", ""), "path": path, "mtime": mtime})
+
+            # Append the post (and its attributes) to the list of posts
+            POSTS.append({"filename": file, "path": path, "mtime": mtime})
 
     # Sort the posts by their modification date (newest first)
     POSTS.sort(key = lambda x: x["mtime"], reverse=True)
@@ -36,13 +43,35 @@ def render_homepage():
 
 def render_posts():
     global ENV
+    markdown = mistune.create_markdown(plugins=['strikethrough', 'footnotes', 'table', 'url', 'task_lists', 'abbr', 'mark', 'insert', 'superscript', 'subscript', 'math', 'spoiler'], escape=False, hard_wrap=True)
 
     # Store the post template in memory
     post_template = ENV.get_template("post.html")
 
     # Render every post
     for post in POSTS:
+        # Read the contents of the post
+        with open(post["path"], "r") as file:
+            content = file.read()
+
+        # If the post does not have frontmatter skip it, otherwise use default values
+        if content.startswith("---"):
+            frontmatter, content = [x for x in content.split("---", 2) if x != ""]
+            frontmatter = yaml.safe_load(frontmatter)
+        else:
+            print(f"The post {post["filename"]} does not have frontmatter. Using default values instead.")
+            frontmatter = {}
+
+        # Add the frontmatter attributes to the post's state
+        post["title"] = frontmatter.get("title", post["filename"].replace(".md", ""))
+        post["slug"] = frontmatter.get("slug", post["filename"].lower().replace(" ", "-"))
+        post["created"] = frontmatter.get("created", post["mtime"])
+        post["content"] = markdown(content)
+
+        # If the post has been updated, 
+
         # Render the post using the template
+        print(post)
         output = post_template.render(post=post)
 
         # Create the path of the output render and make the directory
@@ -50,7 +79,7 @@ def render_posts():
         os.makedirs(path, exist_ok=True)
 
         # Write the render to a file
-        with open(f"{os.path.join(path, post["title"])}.html", "w") as file:
+        with open(f"{os.path.join(path, post["slug"])}.html", "w") as file:
             file.write(output)
         
         print(f"The page for the post {post["title"]} has successfully been created.")
--- public/js/projects.js
+++ public/js/projects.js
@@ -7,30 +7,39 @@ document.addEventListener('DOMContentLoaded', () => {
         // Handle navbar categories
         const categories = document.querySelectorAll('div.container');
         navbar.forEach((nav, i) => {
-            // Highlight the category on hover
-            nav.addEventListener('mouseover', () => nav.classList.add('navbar-selected'));
+            // Handle the games anchor separately
+            if (nav.id == "games") {
+                // Highlight the category on hover
+                nav.addEventListener('mouseover', () => nav.classList.add('navbar-selected'));
 
-            // Unhighlight the category on mouse exit
-            nav.addEventListener('mouseout', () => {if (!categories[i].checkVisibility()) nav.classList.remove('navbar-selected')});
+                // Unhighlight the category on mouse exit
+                nav.addEventListener('mouseout', () => nav.classList.remove('navbar-selected'));
+            } else {
+                // Highlight the category on hover
+                nav.addEventListener('mouseover', () => nav.classList.add('navbar-selected'));
 
-            // Show the category elements on click
-            nav.addEventListener('click', (e) => {
-                // Prevent default behaviour of anchor tags
-                e.preventDefault();
+                // Unhighlight the category on mouse exit
+                nav.addEventListener('mouseout', () => {if (!categories[i].checkVisibility()) nav.classList.remove('navbar-selected')});
 
-                // Unselect and hide all categories
-                navbar.forEach((n) => n.classList.remove('navbar-selected'));
-                categories.forEach((category) => category.style.display = 'none');
+                // Show the category elements on click
+                nav.addEventListener('click', (e) => {
+                    // Prevent default behaviour of anchor tags
+                    e.preventDefault();
 
-                // Select the clicked category
-                nav.classList.add('navbar-selected');
+                    // Unselect and hide all categories
+                    navbar.forEach((n) => n.classList.remove('navbar-selected'));
+                    categories.forEach((category) => category.style.display = 'none');
 
-                // Make the corresponding category visible
-                categories[i].style.display = 'block';
+                    // Select the clicked category
+                    nav.classList.add('navbar-selected');
 
-                // Push the new URL to history
-                window.history.pushState({}, '', `?view=${nav.id}`)
-            });
+                    // Make the corresponding category visible
+                    categories[i].style.display = 'block';
+
+                    // Push the new URL to history
+                    window.history.pushState({}, '', `?view=${nav.id}`)
+                });
+            }
         });
 
         // Set the search parameters
