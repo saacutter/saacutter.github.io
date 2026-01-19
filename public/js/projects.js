fe369bf13ac7b2213c7e4d1df5229c4fc9dbd38f
saacutter <168063553+saacutter@users.noreply.github.com>
2026-01-19 21:15:59 +0800

    Fixed navigation button functions on the homepage

--- public/js/projects.js
+++ public/js/projects.js
@@ -1,62 +1,59 @@
 document.addEventListener('DOMContentLoaded', () => {
     const navbar = document.querySelectorAll('nav a');
 
+    // Handle navbar categories
+    const categories = document.querySelectorAll('div.container');
+    navbar.forEach((nav, i) => {
+        // Highlight the category on hover
+        nav.addEventListener('mouseover', () => nav.classList.add('navbar-selected'));
+
+        // Unhighlight the category on mouse exit
+        if (window.location.pathname == "/" && nav.id != "games") nav.addEventListener('mouseout', () => {if (!categories[i].checkVisibility()) nav.classList.remove('navbar-selected')});
+        else nav.addEventListener('mouseout', () => nav.classList.remove('navbar-selected'));
+
+        // Show the category elements on click
+        nav.addEventListener('click', (e) => {
+            if (window.location.pathname != "/" || nav.id == "games") return;
+
+            // Prevent default behaviour of anchor tags
+            e.preventDefault();
+
+            // Unselect and hide all categories
+            navbar.forEach((n) => n.classList.remove('navbar-selected'));
+            categories.forEach((category) => category.style.display = 'none');
+
+            // Select the clicked category
+            nav.classList.add('navbar-selected');
+
+            // Make the corresponding category visible
+            categories[i].style.display = 'block';
+
+            // Change the page title to reflect the current view (adapted from https://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript)
+            document.title = `saacutter.github.io - ${nav.id.charAt(0).toUpperCase() + nav.id.substring(1)}`;
+
+            // Push the new URL to history (adapted from https://stackoverflow.com/questions/3528324/how-to-get-the-previous-url-in-javascript)
+            if (window.history.state == null || window.history.state.prevView != nav.id) window.history.pushState({prevView: nav.id}, '', `?view=${nav.id}`);
+        });
+    });
+
+    // Set the view to the previous page if the back buttons are used
+    window.addEventListener('popstate', (e) => {
+        if (e.state) setView(e.state.prevView);
+        else setView(null);
+    });
+
+    // Set the search parameters
     if (window.location.pathname == "/") {
-        const url = new URL(window.location.href);
-
-        // Handle navbar categories
-        const categories = document.querySelectorAll('div.container');
-        navbar.forEach((nav, i) => {
-            // Handle the games anchor separately
-            if (nav.id == "games") {
-                // Highlight the category on hover
-                nav.addEventListener('mouseover', () => nav.classList.add('navbar-selected'));
-
-                // Unhighlight the category on mouse exit
-                nav.addEventListener('mouseout', () => nav.classList.remove('navbar-selected'));
-            } else {
-                // Highlight the category on hover
-                nav.addEventListener('mouseover', () => nav.classList.add('navbar-selected'));
-
-                // Unhighlight the category on mouse exit
-                nav.addEventListener('mouseout', () => {if (!categories[i].checkVisibility()) nav.classList.remove('navbar-selected')});
-
-                // Show the category elements on click
-                nav.addEventListener('click', (e) => {
-                    // Prevent default behaviour of anchor tags
-                    e.preventDefault();
-
-                    // Unselect and hide all categories
-                    navbar.forEach((n) => n.classList.remove('navbar-selected'));
-                    categories.forEach((category) => category.style.display = 'none');
-
-                    // Select the clicked category
-                    nav.classList.add('navbar-selected');
-
-                    // Make the corresponding category visible
-                    categories[i].style.display = 'block';
-
-                    // Change the page title to reflect the current view (adapted from https://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript)
-                    document.title = `saacutter.github.io - ${nav.id.charAt(0).toUpperCase() + nav.id.substring(1)}`;
-
-                    // Push the new URL to history
-                    window.history.pushState({}, '', `?view=${nav.id}`)
-                });
-            }
-        });
-
-        // Set the search parameters
-        const params = url.searchParams;
-        let view = params.get('view') || 'home';
-        if (document.querySelector(`#${view}`) == null) view = 'home';
-        document.querySelector(`#${view}`).click();
-    } else {
-        navbar.forEach((nav) => {
-            // Highlight the category on hover
-            nav.addEventListener('mouseover', () => nav.classList.add('navbar-selected'));
-
-            // Unhighlight the category on mouse exit
-            nav.addEventListener('mouseout', () => nav.classList.remove('navbar-selected'));
-        });
+        setView(null);
     }
 });
+
+function setView(view) {
+    if (view == null) {
+        let url = new URL(window.location.href);
+        let params = url.searchParams;
+        view = params.get('view');
+        if (document.querySelector(`#${view}`) == null) view = 'home';
+    }
+    document.querySelector(`#${view}`).click();
+}
